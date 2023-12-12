28f7c2008969a67f2698197f93bba7558a746878
Arunajith056 <arunajith056@gmail.com>
2023-12-12 11:37:46 +0530

    add js

--- to-do-list/index.js
+++ to-do-list/index.js
@@ -1,78 +1,63 @@
-// Wait for the DOM to be fully loaded before executing any JavaScript
 document.addEventListener("DOMContentLoaded", function () {
+  const myForm = document.getElementById("fm");
+  const myList = document.getElementById("myList");
+  const inputField = document.getElementById("inp");
 
-    // Get a reference to the form and unordered list
-    let myForm = document.getElementById("fm");
-    let myList = document.getElementById("myList");
-    let inputField = document.getElementById("inp");
-  
-    // Add an event listener to the form that listens for when it's submitted
-    myForm.addEventListener("submit",(event) => {
-      // Prevents the default form submission behavior
+  myForm.addEventListener("submit", function (event) {
       event.preventDefault();
-  
-      // Get the input value from the text input field
-      const inputValue = inputField.value;
-  
-      // Create a new list item element
-      const newListItem = document.createElement("li");
-  
-      // Set its text content to be equal to what was entered in the text input field
-      newListItem.textContent = inputValue;
-  
-      // Create a delete button element
-      const deleteButton = document.createElement("button");
-      
-      // Set its text content to "delete"
-      deleteButton.textContent = "delete";
-  
-      // Add an event listener to listen for when this button is clicked
-      deleteButton.addEventListener("click", function () {
-        // Remove this list item from its parent unordered list when clicked
-        newListItem.remove();
-      });
-  
-      // Append this delete button element as a child of our new list item element 
-      newListItem.appendChild(deleteButton);
-  
-      // Append our new list item element as a child of our unordered list
-      myList.appendChild(newListItem);
-  
-      // Clear the text input field
-      inputField.value = "";
-    });
-  
-    // Add an event listener to the button that listens for when it's clicked
-    document.getElementById("bt").addEventListener("click", function() {
-      // Get the input value from the text input field
+
       const inputValue = inputField.value.trim();
-  
-      // Create a new list item element
-      const newListItem = document.createElement("li");
-  
-      // Set its text content to be equal to what was entered in the text input field
-      newListItem.textContent = inputValue;
-  
-      // Create a delete button element
-      const deleteButton = document.createElement("button");
-      
-      // Set its text content to "delete"
-      deleteButton.textContent = "delete";
-  
-      // Add an event listener to listen for when this button is clicked
-      deleteButton.addEventListener("click", function () {
-        // Remove this list item from its parent unordered list when clicked
-        newListItem.remove();
-      });
-  
-      // Append this delete button element as a child of our new list item element 
-      newListItem.appendChild(deleteButton);
-  
-      // Append our new list item element as a child of our unordered list
-      myList.appendChild(newListItem);
-  
-      // Clear the text input field
-      inputField.value = "";
-    });
+
+      if (inputValue !== "") {
+          const newRow = document.createElement("tr");
+
+          const textCell = document.createElement("td");
+          textCell.textContent =inputValue;
+
+          const buttonCell = document.createElement("td");
+          const deleteButton = document.createElement("button");
+
+          deleteButton.classList.add("btn", "btn-danger");
+          deleteButton.textContent = "Delete";
+
+          deleteButton.addEventListener("click", function () {
+              newRow.remove();
+          });
+
+          buttonCell.appendChild(deleteButton);
+          newRow.appendChild(textCell);
+          newRow.appendChild(buttonCell);
+          myList.appendChild(newRow);
+
+          inputField.value = "";
+      }
   });
-  
+
+  document.getElementById("bt").addEventListener("click", function () {
+      const inputValue = inputField.value.trim();
+
+      if (inputValue !== "") {
+          const newListItem = document.createElement("tr");
+          const textCell = document.createElement("td");
+
+          textCell.textContent = inputValue;
+
+          const deleteButtonCell = document.createElement("td");
+          const deleteButton = document.createElement("button");
+
+          deleteButton.classList.add("btn", "btn-danger");
+          deleteButton.textContent = "Delete";
+
+          deleteButton.addEventListener("click", function () {
+              newListItem.remove();
+          });
+
+          deleteButtonCell.appendChild(deleteButton);
+          newListItem.appendChild(textCell);
+          newListItem.appendChild(deleteButtonCell);
+          myList.appendChild(newListItem);
+
+          inputField.value = "";
+      }
+  });
+});
